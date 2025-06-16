3fa588fb6f5acb41be3fb8709d5f0ce0c16695a6
vijay rajesh <ewe111.vijay@gmail.com>
2025-06-16 11:52:00 +0500

    ...

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+    domains: ['placehold.co'], // <-- Add this line
+  },
+};
 
 export default nextConfig;
